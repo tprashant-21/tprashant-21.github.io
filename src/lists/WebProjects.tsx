fd88f6a9ff523463d444e19fb5dcf0c6b16279c8
Prashant Timalsina <62254242+tprashant-21@users.noreply.github.com>
2024-10-11 14:41:26 -0400

    Update WebProjects.tsx

--- src/lists/WebProjects.tsx
+++ src/lists/WebProjects.tsx
@@ -3,7 +3,7 @@ export const WebProjects = [
         id: 1,
         title: "Motion Tracker Lab",
         description: "A motion tracking system that utilizes a webcam and a computer vision algorithm to accurately track the position of an object undergoing planar motion as a function of time. The ultimate goal of our project is to create a completely self contained open-source system that educators can use to help students explore one dimensional motion in a physics lab setting. ",
-        detail: "Me, Professor Jeremy Castagno, and Professor Jeff Gagnon are working on it. We presented the demo at “Scholars in Action” in April at Springfield College. I am responsible for the UI/UX design and entire frontend system.",
+        detail: "Me, Professor Jeremy Castagno, and Professor Jeff Gagnon are working on it. We presented the demo at “Scholars in Action” in April at Springfield College.",
         tech: ["REACT", "TypeScript", "Zustand", "Tailwind", "React Router", "OpenCV"],
         github: "https://github.com/PiPhysics/MotionTrackerLab",
         live: "https://piphysics.github.io/MotionTrackerLab/",
@@ -12,7 +12,7 @@ export const WebProjects = [
     {
         id: 2,
         title: "Giclée Printing and Framing of New England website",    
-        description: "I along with my group of developer and designer friends completely revamped the website for this local business in the area as an experiential learning project. Before we made our hands dirty the website had a lot of html pages and an unprofessional look to it.  ",
+        description: "I along with my group of developer and designer friends completely revamped the website for a local print shop in the area. Before we made our hands dirty the website had a lot of webpages and an unprofessional look to it.",
         detail: "Our goal was to make a beautiful responsive website that speaks with its audience. And, we are glad that we were able to deliver it. ",
         tech: ["AlpineJS", "TailwindCSS", "HTML", "CSS", "Zola"],   
         github: "https://github.com/SpringfieldCollege-MPCS/cisc288-final-website-giclee-printing-framing",
@@ -22,8 +22,8 @@ export const WebProjects = [
     {
         id: 3,
         title: "effective-task-planner",
-        description: "Not your average task-planner! The effective-task-planner app digitalises the task planning proces that is the most saught after among productive humans of today's world. I used to print the paper with template and use it. But here's my attempt to turn it into an app, and increase productivity in all of us. IT REALLYY HELPS Y'ALL!",
-        detail :"Through this project, I expanded my skill set and gained the ability to develop a comprehensive React application with local state management system. This experience allowed me to create an aesthetically pleasing and highly functional solution to my problem.",
+        description: "Not your average task-planner! The effective-task-planner app digitalises the task planning proces that is the most saught after among productive humans of today's world. I used to print the template paper and use it. But here's my attempt to turn it into an app, and increase productivity in all of us.",
+        detail :"Through this project, I expanded my skill set and gained the ability to develop a comprehensive React application with local state management system. This experience allowed me to create an aesthetically pleasing and highly functional solution to my own problem.",
         tech: ["REACT", "TypeScript", "TailwindCSS", "React DnD"],
         github: "https://github.com/tprashant-21/effective-task-planner",
         live: "https://effective-task-planner.vercel.app/",
