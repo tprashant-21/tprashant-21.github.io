4fae03ba98164083dadeaf7030e0b131115ae338
Prashant Timalsina <62254242+tprashant-21@users.noreply.github.com>
2024-10-14 19:57:16 -0400

    changes in AnimationProjects.tsx

--- src/lists/AnimationProjects.tsx
+++ src/lists/AnimationProjects.tsx
@@ -5,8 +5,8 @@ export const AnimationProjects = [
         description: "I made this video for final project of my 3D Animation class. I am a massive Michael Jackson fan, so this is sort of a tribute to him.",
         detail: "Through this project, I expanded my skillset in 3D animation, rigging, and modeling. I tried to go out and beyond my course requirement using advanced concepts such as nCloth. I am proud of the result.",
         tech: ["Autodesk Maya", "Maximo"],
-        github: "https://drive.google.com/file/d/1wci6wK8jn7GrXVOSMMbvEm4JA0mKiKb7/view?usp=drive_link",
-        live: "https://drive.google.com/file/d/1YPJSyq3dBmVHtTcA2bDa3-G2b4Tv7-VS/view?usp=drive_link",
+        github: "https://drive.google.com/file/d/1cgX1kqJfYBz6nW5BUMl0v62heKNVeOga/view?usp=sharing",
+        live: "https://drive.google.com/file/d/1WkzCT7Yp_-tf2TIV1CIu7IkV74h8SVMP/view?usp=sharing",
         image: "/animation-3d.webp"
     },
     {
@@ -15,8 +15,8 @@ export const AnimationProjects = [
         description: "This is based on a real story when I was a toddler. I was so excited to make an animation out of it, for my Computer Animation class. My script, storyboard, animation technique were all inspired by motivated camera.",
         detail: "I lost the sound of this video, but I think it is still a good video. I had a lot of fun making this animation video. I learned a lot about animation, storytelling, and drawing vector graphics.",
         tech: ["Adobe Animate", "Adobe Illustrator"],   
-        github: "https://docs.google.com/document/d/1UcVMi64zKoES_RXP-wYe9KUbS5dH1quZ6D0zjsw9dz0/edit?usp=drive_link",
-        live: "https://drive.google.com/file/d/1B-vzQ1Tk29J22prkr9GKgmGpsAKvgJRQ/view?usp=drive_link",
+        github: "https://docs.google.com/document/d/1QwfYsuNlc-SGlII4vbnDhe7yLwk1X1Ua/edit?usp=sharing&ouid=104242399404156295116&rtpof=true&sd=true",
+        live: "https://drive.google.com/file/d/1dZyGj0QdRrr9bOeupodugK8Tgi6Q9ifd/view?usp=sharing",
         image: "/animation-2d.webp"
     },
     ];
