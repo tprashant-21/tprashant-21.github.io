e673c7063c69be8a370340227e80276da075a385
Prashant Timalsina <62254242+tprashant-21@users.noreply.github.com>
2024-10-14 20:12:15 -0400

    Changes in MotionGraphicsProjects.tsx

--- src/lists/MotionGraphicsProjects.tsx
+++ src/lists/MotionGraphicsProjects.tsx
@@ -5,8 +5,8 @@ export const MotionGraphicsProjects = [
         description: "An kinetic type animation using After Effects for a scene from Rush Hour 3 movie. I made this animation for my Motion Graphics class. Everyone enjoyed it a lot. One of friend even said that he is not going after me to present his work because he knows that his work is not going to be as good as mine. ",
         detail: "I really tried my best to bring life to these letters and words through animation and storytelling.",
         tech: ["After Effects", "Adobe Illustrator"],
-        github: "https://drive.google.com/file/d/1SeqlwrGpd1jPGEW_0zo9BKidFA3Q4E0v/view?usp=drive_link",
-        live: "https://drive.google.com/file/d/1aveON9gWVcay2MOe3uqqendgc-2VByW2/view?usp=drive_link",
+        github: "https://drive.google.com/file/d/1iBM-HYTSqWL3T4SXC33s66LMEY2BVSxA/view?usp=sharing",
+        live: "https://drive.google.com/file/d/1moylTOppe3HK3qA8jnvt2WL3mdld1N2f/view?usp=sharing",
         image: "/motion-im-yu.webp"
     },
     {
@@ -15,8 +15,8 @@ export const MotionGraphicsProjects = [
         description: "An entirely new remake of the title sequence for Jim Henson's film, Labyrinth. Its current title sequence under represents the film, or in other words, does not do it justice.",
         detail: "I did my thorough research on the appropriate imagery, type and format for the titles that best represented the movie. I used a lot of imaginative elements and characters in this film to come up with a vast array of design solutions.",
         tech: ["After Effects", "Adobe Illustrator"],   
-        github: "https://drive.google.com/file/d/1x3ySXEzxRpB67yuzMkKrW9-pMWJRuUhG/view?usp=drive_link",
-        live: "https://drive.google.com/file/d/1ZD_I0h6rtJ5eNJCKG65zkhAO2zg-NP97/view?usp=drive_link",
+        github: "https://drive.google.com/file/d/1b2f2r3EcskNdkq4-4EbQCIr2izhAbbfD/view?usp=sharing",
+        live: "https://drive.google.com/file/d/1z79aNLIQ8eZcPIeSjvDY61AbfXeN8J4_/view?usp=sharing",
         image: "/motion-labyrinth.webp"
     },
     {
@@ -25,8 +25,8 @@ export const MotionGraphicsProjects = [
         description: "The animation effectively expresses the movement, rhythm, and pace of the chosen royalty-free audio track by utilizing geometric forms, thereby producing a captivating visual representation of the music.",
         detail: " The use of abstract forms and colors enables the viewer to experience the emotions and energy of the music without being hindered by the presence of lyrics. The animation is composed of different scenes and forms, which provide variety and maintain the viewer's engagement, culminating in a satisfactory and comprehensive ending.",
         tech: ["After Effects"],
-        github: "https://drive.google.com/file/d/1aXHqlDDRgoy1YmvD71k2mmE_cFx0qvef/view?usp=drive_link",
-        live: "https://drive.google.com/file/d/1jPFeT4BtE2DVFSokxxuGsFv5Q5rdU2SW/view?usp=drive_link",
+        github: "https://drive.google.com/file/d/1XDzL2m2UOZET8BKbmKTrlFZTPHXojMH-/view?usp=sharing",
+        live: "https://drive.google.com/file/d/12-BPrKyKYrccupuxs0YuhRExlTKAKy48/view?usp=sharing",
         image: "/motion-geometric.webp"
     },
     {
@@ -35,8 +35,8 @@ export const MotionGraphicsProjects = [
         description: "A fun, creative, expressive stop-motion to represent the strengths of the Department of Visual and Performing Arts, its students, and the work created there.",
         detail: "I collaborated with amazing artists namely Haley Livesay, Jesika Garay, and Rachel Justice to create this video. We used different techniques like cut-out, claymation, and pixilation. I had so much fun making this video especially because I am also in the video lol.",
         tech: ["After Effects"],
-        github: "https://drive.google.com/file/d/1-U1I8uy08bhkRwwWWc-_AaJx9V8pKNlx/view?usp=drive_link",
-        live: "https://drive.google.com/file/d/1p6i7rWeeq66Nr5cwyNt4LN9D2aFb6gFI/view?usp=drive_link",
+        github: "https://drive.google.com/file/d/1l8g3UYcOg2IVhoma701oYsHfsTnjj8g7/view?usp=sharing",
+        live: "https://drive.google.com/file/d/16w6AS0bcgoRhaXOwmcJUIYo8QyVtsAAt/view?usp=sharing",
         image: "/motion-stop-animation.webp"
     },
     {
@@ -45,8 +45,8 @@ export const MotionGraphicsProjects = [
         description: "A video tributing the amazing memories of Cristiano Ronaldo at Real Madrid. I did this project to perfect rhythm and timing in video editing. The audio track is one of my favorite songs, and I wanted to make a video that would do it justice.",
         detail: "I hope to give audience goosebumps with my edit.",
         tech: ["After Effects", "Adobe Illustrator"],
-        github: "https://3.basecamp.com/3589416/buckets/28783280/uploads/5346576344",
-        live: "https://drive.google.com/file/d/1uKVcVaFKcQi_Qr59-DF15GwXyxMdVq7e/view?usp=drive_link",
+        github: "https://drive.google.com/file/d/1SFevB9eLQ9PPgrZV90Yl4eOWyQTEXLBj/view?usp=sharing",
+        live: "https://drive.google.com/file/d/1SFevB9eLQ9PPgrZV90Yl4eOWyQTEXLBj/view?usp=sharing",
         image: "/motion-cr7.webp"
     },
     ];
